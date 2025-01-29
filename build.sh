81135522b8bf5712227c31896cb9f7d9baecf98e
hjw_1112 <jungwoo_hong@bisb.sk>
2025-01-30 00:00:46 +0100

    deploy update

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
+set -o errexit 
+
+apt-get update && apt-get install -y \
+    tesseract-ocr \
+    libtesseract-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+pip install --no-cache-dir -r requirements.txt
